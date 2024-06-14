bfc6e1a79f150bc612300ce6795f2c4c894e070a
Dave Duprey <dave@what3words.com>
2024-06-14 15:35:31 +0100

    added some event support

---
+++ Sources/W3WSwiftCore/Reactive/W3WEventSubscriberProtocol.swift
@@ -0,0 +1,39 @@
+//
+//  EventSubscriberProtocol.swift
+//  TestApp
+//
+//  Created by Dave Duprey on 13/03/2024.
+//
+
+#if canImport(Combine)
+import Combine
+#endif
+
+
+@available(iOS 13.0, macOS 10.15, watchOS 6.0, tvOS 13.0, *)
+public protocol W3WEventSubscriberProtocol: AnyObject {
+  
+  var subscriptions: W3WEventsSubscriptions { get set }
+  
+}
+
+
+@available(iOS 13.0, macOS 10.15, watchOS 6.0, tvOS 13.0, *)
+public extension W3WEventSubscriberProtocol {
+  
+  func subscribe<EventType: Subject>(to: EventType?, handler: @escaping (EventType.Output) -> ()) {
+    let subscription = to?.sink(
+      receiveCompletion: { _ in },
+      receiveValue: { event in  handler(event) })
+    
+    if let s = subscription {
+      subscriptions.insert(s)
+    }
+  }
+  
+  
+  func type(for type: Any.Type) -> String {
+    return String(describing: type)
+  }
+  
+}
