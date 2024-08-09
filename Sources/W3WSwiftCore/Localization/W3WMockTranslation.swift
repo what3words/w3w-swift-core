6bb10e7f6cf19f0f72dfebd10dada85c54375e87
Khai Do <khai.dohuynh@what3words.com>
2024-08-09 16:38:46 +0700

    add W3WMockTranslation

---
+++ Sources/W3WSwiftCore/Localization/W3WMockTranslation.swift
@@ -0,0 +1,15 @@
+//
+//  W3WMockTranslation.swift
+//  
+//
+//  Created by Khải Toàn Năng on 9/8/24.
+//
+
+import Foundation
+
+public class W3WMockTranslation: W3WTranslationsProtocol {
+  public init() {}
+  public func get(id: String) -> String {
+    ""
+  }
+}
