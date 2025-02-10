bdc104ac62292a3aecef3f2a92c6e9ac3df2003a
Khai Do <khai.dohuynh@what3words.com>
2025-02-10 14:27:04 +0700

    [MT-7226] Add Box Id

--- Sources/W3WSwiftCore/Types/Geometry/W3WBox.swift
+++ Sources/W3WSwiftCore/Types/Geometry/W3WBox.swift
@@ -1,6 +1,6 @@
 //
-//  File.swift
-//  
+//  W3WBox.swift
+//
 //
 //  Created by Dave Duprey on 01/11/2022.
 //
@@ -12,6 +12,28 @@ import CoreLocation
 public protocol W3WBox {
   var southWest: CLLocationCoordinate2D { get }
   var northEast: CLLocationCoordinate2D { get }
+}
+
+public extension W3WBox {
+  /// Unique ID of the bounds
+  var id: Int64 {
+      let sw = self.southWest
+      let ne = self.northEast
+
+      guard (-90...90).contains(sw.latitude), (-90...90).contains(ne.latitude) else {
+          fatalError("Invalid latitude value: must be between -90 and 90")
+      }
+      guard (-180...180).contains(sw.longitude), (-180...180).contains(ne.longitude) else {
+          fatalError("Invalid longitude value: must be between -180 and 180")
+      }
+
+      let swLatBits = Int64((sw.latitude * 1e6).rounded()) & 0x7FFFFFFF
+      let swLngBits = Int64((sw.longitude * 1e6).rounded()) & 0x7FFFFFFF
+      let neLatBits = Int64((ne.latitude * 1e6).rounded()) & 0x7FFFFFFF
+      let neLngBits = Int64((ne.longitude * 1e6).rounded()) & 0x7FFFFFFF
+
+      return (swLatBits << 48) | (swLngBits << 32) | (neLatBits << 16) | neLngBits
+  }
 }
 
 
