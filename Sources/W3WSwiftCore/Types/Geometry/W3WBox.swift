caacc95cd1f1f8e3eb133c9fbdc55e5e178a5c04
Henry Ng <henry.nguyen@what3words.com>
2025-04-01 11:12:44 +0700

    add square property

--- Sources/W3WSwiftCore/Types/Geometry/W3WBox.swift
+++ Sources/W3WSwiftCore/Types/Geometry/W3WBox.swift
@@ -18,24 +18,32 @@ public extension W3WBox {
   /// Unique ID of the bounds
   var id: Int64 {
       let sw = self.southWest
-      let ne = self.northEast
-
+     let ne = self.northEast
+//
       guard (-90...90).contains(sw.latitude), (-90...90).contains(ne.latitude) else {
           fatalError("Invalid latitude value: must be between -90 and 90")
       }
       guard (-180...180).contains(sw.longitude), (-180...180).contains(ne.longitude) else {
           fatalError("Invalid longitude value: must be between -180 and 180")
       }
-
-      let swLatBits = Int64((sw.latitude * 1e6).rounded()) & 0x7FFFFFFF
+//
+      let swLatBits = Int64((sw.latitude * 1e6).rounded()) & 0x7FFFFFFF // 31 bits
       let swLngBits = Int64((sw.longitude * 1e6).rounded()) & 0x7FFFFFFF
       let neLatBits = Int64((ne.latitude * 1e6).rounded()) & 0x7FFFFFFF
       let neLngBits = Int64((ne.longitude * 1e6).rounded()) & 0x7FFFFFFF
 
       return (swLatBits << 48) | (swLngBits << 32) | (neLatBits << 16) | neLngBits
+    
+    //   let swLatBits = Int64((sw.latitude * 1e6).rounded()) & 0x7FFFF  // 15 bits
+   //    let swLngBits = Int64((sw.longitude * 1e6).rounded()) & 0x7FFFF // 15 bits
+    //   let neLatBits = Int64((ne.latitude * 1e6).rounded()) & 0x7FFFF  // 15 bits
+   //    let neLngBits = Int64((ne.longitude * 1e6).rounded()) & 0x7FFFF // 15 bits
+
+       // Shift by smaller amounts to avoid overflow
+   //    return (swLatBits << 42) | (swLngBits << 28) | (neLatBits << 14) | neLngBits
   }
 }
-
+	
 
 /// Helper object representing a W3W bounding box
 public struct W3WBaseBox: W3WBox {
