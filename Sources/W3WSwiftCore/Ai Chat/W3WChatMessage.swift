f83296b1e94ae825b027f0480bee995e3bb56843
Henry Ng <henry.nguyen@what3words.com>
2025-06-14 21:10:26 +0700

    add ChatRequest type

---
+++ Sources/W3WSwiftCore/Ai Chat/W3WChatMessage.swift
@@ -0,0 +1,16 @@
+//
+//  W3WChatMessage.swift
+//  w3w-swift-core
+//
+//  Created by Henry Ng on 14/6/25.
+//
+
+public struct ChatMessage: Codable, Sendable {
+    public let role: String
+    public let content: String
+    
+    public init(role: String, content: String) {
+        self.role = role
+        self.content = content
+    }
+}
